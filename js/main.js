e1e54ad55a22698cc0f20b9408dc9be04dd27dbd
PPnostalgia <1007254538@qq.com>
2021-12-18 15:04:39 +0800

    add custom.js

--- js/main.js
+++ js/main.js
@@ -866,6 +866,22 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+function switchDarkMode () { // Switch Between Light And Dark Mode
+  const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+  if (nowMode === 'light') {
+    activateDarkMode()
+    saveToLocal.set('theme', 'dark', 2)
+    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+  } else {
+    activateLightMode()
+    saveToLocal.set('theme', 'light', 2)
+    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+  }
+  // handle some cases
+  typeof utterancesTheme === 'function' && utterancesTheme()
+  typeof FB === 'object' && window.loadFBComment()
+  window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+}
 /*
 document.getElementById('mode-button').addEventListener('click', function () { setTimeout(switchPostChart, 100) })
 document.getElementById('mode-button').addEventListener('click', function () { setTimeout(switchVisitChart, 100) })
